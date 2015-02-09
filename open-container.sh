b37cd1e9327350f4a730fd930abbe7a85438ce56
Ingmar Pörner <ipoerner@gmail.com>
2015-02-09 11:49:42 +0100

    credit where credit is due

--- open-container.sh
+++ open-container.sh
@@ -13,4 +13,6 @@ PRIMARY_DNS=`nm-tool | awk '/DNS:/ { print $2 }'`
 USER_ID=`id -u`
 GROUP_ID=`id -g`
 
+# the trick of forwarding the SSH agent into the Docker container comes from Docker issue #6396 on GitHub
+# however, it was most probably first introduced by Chris Corbyn <https://gist.github.com/d11wtq/8699521>
 sudo docker run --privileged -e DEV_UID=${USER_ID} -e DEV_GID=${GROUP_ID} --interactive --tty --dns ${PRIMARY_DNS} --name ${CONTAINER_NAME} --volume ${REPOSITORY_PATH}:${REMOTE_WORKDIR} --volume "$SSH_AUTH_SOCK:/tmp/ssh_auth_sock" -e "SSH_AUTH_SOCK=/tmp/ssh_auth_sock" --rm ${IMAGE_NAME}
