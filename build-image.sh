1a0df8f641db4cad31b5ca04b3ec35dee21e3fed
Ingmar Pörner <ipoerner@gmail.com>
2015-02-09 10:39:22 +0100

    fixes issues with paths within Docker image names
    the build script now accepts a second parameter that indicates whether or not
    the image getting built is from the 'public' or 'private' space

--- build-image.sh
+++ build-image.sh
@@ -3,18 +3,19 @@
 set -e
 
 PROJECT=${1:-default}
+VISIBILITY=${2:-public}
 IMAGE_NAME="build-env-${PROJECT}"
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-mkdir -p ${SCRIPTPATH}/${PROJECT}/env
-cp -f ${SCRIPTPATH}/env/* ${SCRIPTPATH}/${PROJECT}/env/
+mkdir -p ${SCRIPTPATH}/${VISIBILITY}/${PROJECT}/env
+cp -f ${SCRIPTPATH}/env/* ${SCRIPTPATH}/${VISIBILITY}/${PROJECT}/env/
 
-mkdir -p ${SCRIPTPATH}/${PROJECT}/config
+mkdir -p ${SCRIPTPATH}/${VISIBILITY}/${PROJECT}/config
 (cd ${SCRIPTPATH}/config
-for file in `ls -AL`; do
-    cp -f $file ${SCRIPTPATH}/${PROJECT}/config/
+for file in `ls -AL -I .gitignore`; do
+    cp -f $file ${SCRIPTPATH}/${VISIBILITY}/${PROJECT}/config/
 done
 )
 
-sudo docker build --tag ${IMAGE_NAME} --force-rm ${SCRIPTPATH}/${PROJECT}
+sudo docker build --tag ${IMAGE_NAME} --force-rm ${SCRIPTPATH}/${VISIBILITY}/${PROJECT}
